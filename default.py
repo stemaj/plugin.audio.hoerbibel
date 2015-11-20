9d8bdc02463f9ad34f56284220c0d59f4cf0df7c
stemaj <steffen.majaura@gmail.com>
2015-11-20 16:26:16 +0100

    [ADD] first version

---
+++ default.py
@@ -0,0 +1,95 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import time
+import xbmc
+import xbmcgui
+import xbmcaddon
+import xbmcplugin
+import socket
+import urllib
+import hoerBibelCore
+
+addonID = 'plugin.audio.hoerbibel'
+addon = xbmcaddon.Addon(id=addonID)
+socket.setdefaulttimeout(30)
+pluginhandle = int(sys.argv[1])
+translation = addon.getLocalizedString
+addonDir = xbmc.translatePath(addon.getAddonInfo('path'))
+icon = os.path.join(addonDir ,'icon.png')
+tempPath = addon.getSetting("tempPath")
+
+if (len(tempPath) == 0):
+    nameAddon = "Hörbibel" # translation(30101) #"Hörbibel"
+    bittePfad = "Bitte Pfad in Einstellungen festlegen" # translation(30103) #"Bitte Pfad in Einstellungen festlegen"
+    xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(nameAddon, bittePfad, 4500, icon))
+    sys.exit(0)
+    
+def index():
+    books = hoerBibelCore.getBooks()
+    for book in books:
+        addDir(book, book, "listChapters")
+    #addDir('Rut', 'http://www.bibleserver.com/text/ABV/Rut1', "listChapters")
+    xbmcplugin.endOfDirectory(pluginhandle)
+
+def listChapters(book):
+    cnt = hoerBibelCore.chapterCount(book)
+    for i in range(1, cnt, 1):
+        addLink(str(i), book, 'playAudio')
+    xbmcplugin.endOfDirectory(pluginhandle)
+
+def addDir(name, url, mode):
+    u = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+urllib.quote_plus(mode)+"&fanart="+urllib.quote_plus(icon)
+    ok = True
+    liz = xbmcgui.ListItem(name, icon, thumbnailImage=icon)
+    liz.setInfo(type="Audio", infoLabels={"Title": name})
+    ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=True)
+    return ok
+
+def addLink(name, url, mode):
+    u = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+urllib.quote_plus(mode)+"&name="+urllib.quote_plus(name)
+    liz=xbmcgui.ListItem(name, iconImage="DefaultAudio.png", thumbnailImage=icon)
+    liz.setInfo(type="Audio", infoLabels={"Title": name})
+    liz.setProperty('IsPlayable', 'true')
+    xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)
+
+def playAudio(book,chapter):
+    import SimpleDownloader as downloader
+    downloader = downloader.SimpleDownloader()
+    link = hoerBibelCore.getAudioLink(book,chapter)
+    params = { "url": link, "download_path": tempPath }
+    tempFileName = "myvideo.mp3"
+    if os.path.isfile(tempPath + tempFileName):
+        os.remove(tempPath + tempFileName)
+    downloader.download(tempFileName, params)
+    while not os.path.isfile(tempPath + tempFileName):
+        time.sleep(.100)
+    listitem = xbmcgui.ListItem(path= tempPath + tempFileName)
+    listitem.setInfo( type="Audio", infoLabels={ "Title": name } )
+    listitem.setProperty("mimetype", 'audio/mpeg')
+    return xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
+
+def parameters_string_to_dict(parameters):
+    paramDict = {}
+    if parameters:
+        paramPairs = parameters[1:].split("&")
+        for paramsPair in paramPairs:
+            paramSplits = paramsPair.split('=')
+            if (len(paramSplits)) == 2:
+                paramDict[paramSplits[0]] = paramSplits[1]
+    return paramDict
+
+params = parameters_string_to_dict(sys.argv[2])
+mode = urllib.unquote_plus(params.get('mode', ''))
+url = urllib.unquote_plus(params.get('url', ''))
+name = urllib.unquote_plus(params.get('name', ''))
+fanart = urllib.unquote_plus(params.get('fanart', ''))
+
+if mode == "playAudio":
+    playAudio(url,name)
+elif mode == "listChapters":
+    listChapters(url)
+else:
+    index()
---
+++ hoerBibelCore.py
@@ -0,0 +1,71 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import urllib2
+import re
+import httplib
+import socket
+
+def getUrl(url):
+    error = ''
+    link = ''
+    req = urllib2.Request(url, headers={'accept': '*/*'})
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:19.0) Gecko/20100101 Firefox/19.0')
+    try:
+        response = urllib2.urlopen(req)
+        if not response:
+            error = 'No response - Please try again'
+    except urllib2.HTTPError as e:
+        error = 'Error code: ', e.code
+    except urllib2.URLError as e:
+        error = 'Reason: ', e.reason
+    except Exception as e:
+        if e.message:
+            error = e.message
+        else:
+            error = 'Other reason'
+    if not error:
+        try:
+            link = response.read()
+            if not link:
+                error = 'No data - Please try again'
+        except httplib.IncompleteRead as e:
+            error = e.message
+        except Exception as e:
+            error = e.message
+    
+    if not error:
+        if response:
+            response.close()
+
+    return (link, error)
+
+def getBooks():
+    bookSite = getUrl('http://www.bibleserver.com/overlay/selectBook')
+    if not bookSite[0]:
+        return bookSite[1]
+    bookSite = bookSite[0].split("booknames\":{")
+    bookSite = bookSite[1].split("},\"trl_cols")
+    bookSite = bookSite[0]
+    bookSite = bookSite.split(',')
+    books = []
+    for entry in bookSite:
+        books.append(str(entry.split(':')[1]).replace("\"",""))
+    return books
+
+def chapterCount(book):
+    return 50
+
+def getAudioLink(book, chapter):
+    link = getUrl('http://www.bibleserver.com/text/ABV/' + book + str(chapter))
+    link = link[0].split('file: \'')
+    link = link[1].split('config:')
+    link = link[0].split('\',')[0]
+    link = 'http://m.bibleserver.com/' + str(link)
+    return link
+
+#getBooks()
+#link = getAudioLink('1.Könige',7)
+
+#i = 5
+
